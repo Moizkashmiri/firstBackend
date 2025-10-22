b016f82e90f306c839e9134b7a09a3cc9c88f7c9
apple <kashmirimoiz17@gmail.com>
2025-10-22 14:36:27 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get("/moiz", (req, res) => {
+    res.send("henaaaaaaaa");
+})
+app.get("/check", (req, res) => {
+    res.send("Bana liya bhai tune Pehla api");
+})
+
+app.get("/login", (req, res) => {
+    res.send("practice karooooo");
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port ${process.env.PORT}`)
+})
